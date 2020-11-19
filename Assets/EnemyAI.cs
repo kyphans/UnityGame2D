83ba64fcb3be10648b8f35562b5111f6da7dea9c
Huynh Le Kim Ngan <46023648+silverbot99@users.noreply.github.com>
2020-11-19 15:44:50 +0700

    enemy and AI 50%

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+
+    public Path path;
+    int currentWaypopint = 0;
+    bool reachedEndOfPath = false;
+    public Seeker seeker;
+    Rigidbody2D rb;
+
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        seeker.StartPath(rb.position, target.position, OnPathComplete);   
+    }
+
+    void OnPathComplete(Path p){
+        if (!p.error)
+        {
+            path = p;
+            currentWaypopint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
