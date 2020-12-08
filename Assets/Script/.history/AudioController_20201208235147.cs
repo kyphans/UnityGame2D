fdcc8cc9e2cd65abe95507d163b546b2fee55308
ngan51702140 <nhanhuynh.kim23@gmail.com>
2020-12-09 00:16:15 +0700

    audio

---
+++ Assets/Script/.history/AudioController_20201208235147.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource audioCollectDiamond;
+    public AudioSource audioAttack;
+    public AudioSource audioJump;
+    void PlayCollectDiamond()
+    {
+        audioCollectDiamond.Play();
+    }
+    void PlayAttack()
+    {
+        audioAttack.Play();
+    }
+    void PlayJump()
+    {
+        audioJump.Play();
+    }
+}
