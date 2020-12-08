8fb094f6565dccea2f6db0163c63b7dfaf923618
ngan51702140 <nhanhuynh.kim23@gmail.com>
2020-12-08 23:16:11 +0700

    pathfinding

--- Assets/Script/EnemyGFX.cs
+++ Assets/Script/EnemyGFX.cs
@@ -1,21 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;                                                        
 
 public class EnemyGFX : MonoBehaviour
 {
     // Start is called before the first frame update
-    // public AIPath aiPath;
+    public AIPath aiPath;
 
     // // Update is called once per frame
-    // void Update()
-    // {
-    //     if (aiPath.desiredVelocity.x>=0.01f)
-    //     {
-    //         transform.localScale = new Vector3(-1f,1f,1f);
-    //     }
-    //     else if(aiPath.desiredVelocity.x <= -0.01f){
-    //         transform.localScale = new Vector3(1f,1f,1f);
-    //     }
-    // }
+    void Update()
+    {
+         if (aiPath.desiredVelocity.x >= 0.1f)
+        {
+            transform.localScale = new Vector3(-5f, 5f, 5f);
+        }
+        else if (aiPath.desiredVelocity.x <= -0.1f)
+        {
+            transform.localScale = new Vector3(5f, 5f, 5f);
+        }
+    }
 }
